5220468b1239358ee4040fd09caadf62ab65d9a7
walvarado522 <walvarado7821@utm.edu.ec>
2020-12-26 10:41:14 -0500

    Proyecto Añadido Web Personal

---
+++ webpersonal/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+    <h1>Mi Web Personal</h1>
+   <ul>
+      <li><a href="/">Portada</a></li>
+      <li><a href="/about/">Acerca de...</a></li>
+      <li><a href="/portfolio/">Portafolio...</a></li>
+      <li><a href="/contact/">Conctacto...</a></li>
+    </ul>
+"""
+
+# Create your views here.
+def home(request):
+     return render(request,"core/home.html")
+
+def about(request):
+    return render(request,"core/about.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
